c3bf5791a7ff470b3ed60dbbe21728ce2b4b4de2
PriyanshuxChaudhary <ch.priyanshu11@gmail.com>
2024-03-04 03:21:50 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -63,8 +63,6 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors());
-
 
 const corsOptions = {
   origin: 'http://concent.vercel.app', // Replace with your client's domain
